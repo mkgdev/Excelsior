1f34a3703aabac31ce2d4401fbe800313bcd4d6b
mkgdev <mukeshinhere07@gmail.com>
2018-01-09 00:14:50 +0530

    smooth scroll added in events

--- server.js
+++ server.js
@@ -6,17 +6,7 @@ var creds = require('./client/secret_client.json');
 
 var doc = new GoogleSpreadSheet('1DHjj65IDsfXASc41ePe6DHJY2zHNPHIcoj5P1q0guY0');
 
-// Authenticate with the Google Spreadsheets API.
-// doc.useServiceAccountAuth(creds, function (err) {
-//
-//     // Get all of the rows from the spreadsheet.
-//
-//     doc.addRow(1, { Username: 'Agnew', Password: 'Samuel' }, function(err) {
-//         if(err) {
-//             console.log(err);
-//         }
-//     });
-// });
+
 
 
 app.set("view engine", "ejs");
@@ -92,14 +82,27 @@ app.get('/contact',function(req,res) {
 // Registration Routes
 //********************
 
-app.get('/register',function(req,res)
-    {
-
-        res.render('register/register1.ejs');
-    }
-
-
-);
+// app.post('/register',function(req,res)
+//     {
+//
+//         var data = req.body;
+//
+//         Authenticate with the Google Spreadsheets API.
+//         doc.useServiceAccountAuth(creds, function (err) {
+//
+//         // Get all of the rows from the spreadsheet.
+//
+//         doc.addRow(1, { Username: 'Agnew', Password: 'Samuel' }, function(err) {
+//             if(err) {
+//                 console.log(err);
+//             }
+//         });
+//     });
+//
+//     }
+//
+//
+// );
 
 
 
