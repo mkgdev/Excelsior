59e5b4d16b0113d7c1e0ea6794728ddb1589b0bd
sandeepyadav097 <sandeepyadav097@gmail.com>
2018-01-14 11:20:12 +0530

    Important updates

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -5888,7 +5888,7 @@ if (Vel) {
     $(document).on('click.fabClickToggle', '.fixed-action-btn.click-to-toggle > a', function (e) {
       var $this = $(this);
       var $menu = $this.parent();
-      if ($menu.hasClass('active')) {
+      if ($menu.hasClass('activa')) {
         closeFABMenu($menu);
       } else {
         openFABMenu($menu);
@@ -5920,7 +5920,7 @@ if (Vel) {
 
   var openFABMenu = function (btn) {
     var $this = btn;
-    if ($this.hasClass('active') === false) {
+    if ($this.hasClass('activa') === false) {
 
       // Get direction option
       var horizontal = $this.hasClass('horizontal');
@@ -5932,7 +5932,7 @@ if (Vel) {
         offsetY = 40;
       }
 
-      $this.addClass('active');
+      $this.addClass('activa');
       $this.find('ul .btn-floating').velocity({ scaleY: ".4", scaleX: ".4", translateY: offsetY + 'px', translateX: offsetX + 'px' }, { duration: 0 });
 
       var time = 0;
@@ -5955,7 +5955,7 @@ if (Vel) {
       offsetY = 40;
     }
 
-    $this.removeClass('active');
+    $this.removeClass('activa');
     var time = 0;
     $this.find('ul .btn-floating').velocity("stop", true);
     $this.find('ul .btn-floating').velocity({ opacity: "0", scaleX: ".4", scaleY: ".4", translateY: offsetY + 'px', translateX: offsetX + 'px' }, { duration: 80 });
