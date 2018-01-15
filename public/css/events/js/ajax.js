031803f8bbcff4eedc04f0c781062cd4a15b3239
mkgdev <mukeshinhere07@gmail.com>
2018-01-15 21:41:00 +0530

    Blank field Regitration fixed

--- public/css/events/js/ajax.js
+++ public/css/events/js/ajax.js
@@ -7,31 +7,41 @@ $('.submit-event').on('click', function(){
         email : formVal[1].value,
         contactno : formVal[2].value,
         college : formVal[3].value,
-        college: formVal[4].value,
+        year: formVal[4].value,
         course : formVal[5].value
     };
 
+    if(!(data.fullname && data.email && data.contactno && data.college && data.year && course))
+    {
+        alert('Registration Unsuccessfull! All fields are mandatory');
+    }
+    else{
+        var ajaxRequest = $.ajax({
+            url: "/"+url,
+            method: "POST",
+            data:data,
 
-   var ajaxRequest = $.ajax({
-        url: "/"+url,
-        method: "POST",
-        data:data,
-
-    });
+        });
 
 
-    ajaxRequest.done(function(data) {
-        $('.close').click();
-        alert('Registration was successful');
-        $('.'+url).find("input[type=text]").val("");
+        ajaxRequest.done(function(data) {
+            $('.close').click();
+            alert('Registration was successful');
+            $('.'+url).find("input[type=text]").val("");
+            $('.'+url).find("input[type=tel]").val("");
+            $('.'+url).find("input[type=email]").val("");
 
-        // $('.alert').alert()
-    });
+            // $('.alert').alert()
+        });
 
-    ajaxRequest.fail(function(data) {
-        console.dir(data);
-        alert('Registration was unsuccessful. Try again!');
-    });
+        ajaxRequest.fail(function(data) {
+            console.dir(data);
+            alert('Registration was unsuccessful. Try again!');
+        });
+    }
+
+
+
 });
 
 
